7c7f18cdfe55f360123acb2a8b2ea2ad72f50612
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-08-11 10:57:37 +0000

    God is at work man

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in str:
+        cap = ord(i)
+        if cap >= 97 and cap <= 122:
+            cap = chr(cap - 32)
+        else:
+            cap = chr(cap)
+        print("{}".format(cap), end="")
+    print("")
