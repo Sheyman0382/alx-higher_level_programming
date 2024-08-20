9b8ec950cf13af2fefbf0936b61043910a5348e0
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-08-20 02:19:36 +0000

    getting a value of a list index using .pop method

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -5,5 +5,4 @@ def element_in_index(my_list, idx):
     elif idx > len(my_list) - 1:
         return (None)
     else:
-        my_list.pop(idx)
-        return (my_list)
+        return (my_list[idx])
