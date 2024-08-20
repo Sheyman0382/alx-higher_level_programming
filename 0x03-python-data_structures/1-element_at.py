c9025124b84fe912af0601c5ab5fe5e84f137027
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-08-20 03:10:51 +0000

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def element_in_index(my_list, idx):
-    if idx < 0:
-        return None
-    elif idx > len(my_list) - 1:
+    if idx < 0 or idx > len(my_list) - 1:
         return None
     else:
         return my_list[idx]
