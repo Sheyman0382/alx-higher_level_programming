269db64095af2324f693e7f71c44f62957c479dc
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-08-20 03:02:31 +0000

    2-replace_in_list.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def element_in_index(my_list, idx):
     if idx < 0:
-        return (None)
+        return None
     elif idx > len(my_list) - 1:
-        return (None)
+        return None
     else:
-        return (my_list[idx])
+        return my_list[idx]
