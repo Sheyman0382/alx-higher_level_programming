27d1669b9408b4da53a18f88eb4a7e36cf20500a
Sheyman0382 <adegbiteseyi22@gmail.com>
2024-08-22 12:26:41 +0000

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence > 0:
+	ist_leta = (sentene[0],)
+    else:
+        ist_leta = None
     length = (len(sentence),)
     ist_leta = (sentene[0],)
     return length + ist_leta
