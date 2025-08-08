436437fdbaaadeaf1b2f369a34c72a6748887841
Sheyman0382 <adegbiteseyi22@gmail.com>
2025-08-08 04:58:24 +0000

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module that restricts unwanted attribute"""
+
+class LockedClass:
+    """A class whose objects only allow a specific attributes"""
+
+    __slots__ = ["first_name"]
