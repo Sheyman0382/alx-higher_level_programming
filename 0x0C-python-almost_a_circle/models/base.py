6ec2645257e647a916249964612d5d0fafd11582
Sheyman0382 <adegbiteseyi22@gmail.com>
2026-05-03 07:00:37 +0000

    Base class updated with to_json static method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """A base class module"""
+import json
 
 
 class Base:
@@ -14,3 +15,12 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """converts a python object to a json string"""
+
+        if not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
+           
